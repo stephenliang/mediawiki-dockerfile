a7e0214626afaf6e68fdfd265bf5ab4e5dfd7d1c
Pahlevi Fikri Auliya <pahlevi_f@worksap.co.jp>
2015-01-15 19:21:14 +0800

    Change ownership of extensions folder

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@
 # Chown volumes so that nginx can read/write
 chown www-data:www-data /usr/share/nginx/html/images
 chown www-data:www-data /usr/share/nginx/html/LocalSettings.php
-cp -r /tmp/extensions/ /usr/share/nginx/html/extensions/
+chown -R www-data:www-data /tmp/extensions/
+cp -r /tmp/extensions/* /usr/share/nginx/html/extensions/
 
 service php5-fpm start;
 nginx -g "daemon off;"
