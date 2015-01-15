6c5d83373bbec910d3cc0fd9f0593f734e4735a6
Pahlevi Fikri Auliya <pahlevi_f@worksap.co.jp>
2015-01-15 18:51:06 +0800

    Copy additional extensions from /tmp/extensions folder

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 # Chown volumes so that nginx can read/write
 chown www-data:www-data /usr/share/nginx/html/images
 chown www-data:www-data /usr/share/nginx/html/LocalSettings.php
+cp /tmp/extensions /usr/share/nginx/html/extensions
 
 service php5-fpm start;
 nginx -g "daemon off;"
